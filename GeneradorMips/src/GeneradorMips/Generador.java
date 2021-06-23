245e8af1e2704b78701209469dca99a9393a7a57
Esteban <estebanguzra@gmail.com>
2021-06-23 00:12:42 -0600

    Jal Jr y J

--- GeneradorMips/src/GeneradorMips/Generador.java
+++ GeneradorMips/src/GeneradorMips/Generador.java
@@ -96,31 +96,60 @@ public class Generador {
      * Separa los casos de cada linea, no me gusta swich
      */
     private void traducir(){
-        for(String linea:source){
-            if(linea.contains("=")){ //asignaciones
-                text += "#" + linea + "\n";
-                asignaciones(linea);
-            } else if (linea.contains(":") && !linea.contains("#") && !linea.contains("\"")){ //etiqueta
-                text += linea + "\n";
-            } else if (linea.startsWith("#")){ //comentario
-                text += linea + "\n";
-            } else if (linea.contains("return")){
-                returnCase(linea);
-            } else if (linea.contains("write")){
-                text += "#" + linea + "\n";
-                writeCase(linea);
+        for(int i = 0; i < source.length; i++){
+            if(source[i].contains("=")){ //asignaciones
+                text += "#" + source[i] + "\n";
+                asignaciones(source[i],i);
+            } else if (source[i].contains(":") && !source[i].contains("#") && !source[i].contains("\"")){ //etiqueta
+                text += source[i] + "\n";
+            } else if (source[i].startsWith("#")){ //comentario
+                text += source[i] + "\n";
+            } else if (source[i].contains("return")){
+                returnCase(source[i]);
+            } else if (source[i].contains("write")){
+                text += "#" + source[i] + "\n";
+                writeCase(source[i]);
+            } else if(source[i].contains("if ")){
+                text += "#" + source[i] + "\n";
+                ifCase(source[i]);
+            } else if(source[i].contains("goto ")){
+                goCase(source[i]);
+            } else if(source[i].contains("goback")){
+                text += "jr $ra";
+            } else if(source[i].contains("call ")){
+                callCase(source[i]);
             }
         }
     }
     
-    private void asignaciones(String linea){
+    private void callCase(String linea){
+        String destino = linea.replace("call ", "");
+        text += "jal "+ destino + "\n";
+    }
+    
+    private void goCase(String linea){
+        linea = linea.replace("goto ", "").replace(" ", "");
+        text += "j " + linea + "\n";
+    }
+    
+    private void ifCase(String linea){
+        linea = linea.replace("if ", "");
+        String[] lineaIf = linea.split("goto");
+        String condicion = lineaIf[0].replace(" ", "").replace("Int_", "");
+        String destino = lineaIf[1].replace(" ", "");
+        int indexTemp = getPosTempOrPila(condicion); //aqui revisa si está en un temporal o si está en pila y lo carga
+        String condicionMips = "$t" + indexTemp;
+        text+= "beq " + condicionMips + ", 1, " + destino + "\n";
+    }
+    
+    private void asignaciones(String linea,int numeroLinea){
         String[] split = linea.split("=",2);
         String p1 = split[0];
         if(p1.contains("String_")){
             asignacionString(p1,split[1]);
         } else if (p1.contains("Int_")){
-            asignacionInt(p1,split[1]);
-        }
+            asignacionInt(p1,split[1],numeroLinea);
+        } 
     }
     
     private void asignacionString(String p1,String p2){
@@ -128,29 +157,86 @@ public class Generador {
         data += id + " : .asciiz " + p2 + "\n"; 
     }
     
-    private void asignacionInt(String p1,String p2){
+    private void asignacionInt(String p1, String p2, int numeroLinea){
         String id = p1.replace("Int_", "").replace(" ", "");
         if(p2.contains("+")){
             sumaCase(id,p2);
         } else if(p2.contains("-")){
             restaCase(id,p2);
-        } 
+        } else if(p2.contains(">") || p2.contains("<") ||
+                p2.contains("==") || p2.contains("&") ||
+                p2.contains("|")){
+            comparacionesCase(id,p2,numeroLinea);
+        } else {
+            String suma = p2 + " + 0";
+            sumaCase(id,suma);
+        }
+    }
+    
+    private void paramCase(String id, String p2){
+        
+    }
+    
+    private void liCase(String id, String p2){
+        String p2Clear = p2.replaceAll(" ", "");
+        String idMips = "$t" + getTemporalInt(id);
+        text += "li "+ idMips + ", " + p2Clear + "\n"; 
+    }
+    
+    private void comparacionesCase(String id, String p2, int numeroLinea){
+        if(p2.contains(">")){
+           String[] operandos = p2.split(">");
+           String op1 = operandos[0].replace(" ", ""); 
+           String op2 = operandos[1].replace(" ", "");
+           String operando1 = getOperando(op1);
+           String operando2 = getOperando(op2);
+           int temporalInt = getTemporalInt(id);
+           text += "slt $t"+ temporalInt + "," + operando2 + "," + operando1 + "\n"; 
+        } else if(p2.contains("<")){
+           String[] operandos = p2.split("\\<");
+           String operando1 = getOperando(operandos[0].replace(" ", ""));
+           String operando2 = getOperando(operandos[1].replace(" ", ""));
+           int temporalInt = getTemporalInt(id);
+           text += "slt $t"+ temporalInt + "," + operando1 + "," + operando2 + "\n"; 
+        } else if(p2.contains("==")){
+           System.out.println("Aun no está ==");
+        } else if(p2.contains("&")){
+           String[] operandos = p2.split("\\&");
+           String operando1 = getOperando(operandos[0].replace(" ", ""));
+           String operando2 = getOperando(operandos[1].replace(" ", ""));
+           int temporalInt = getTemporalInt(id);
+           text += "and $t"+ temporalInt + "," + operando1 + "," + operando2 + "\n";
+        } else if(p2.contains("|")){
+           String[] operandos = p2.split("\\|");
+           String operando1 = getOperando(operandos[0].replace(" ", ""));
+           String operando2 = getOperando(operandos[1].replace(" ", ""));
+           int temporalInt = getTemporalInt(id);
+           text += "or $t"+ temporalInt + "," + operando1 + "," + operando2 + "\n";
+        }
     }
     
     private void sumaCase(String id,String p2){
         String[] operandos = p2.split("\\+");
         String operando1 = getOperando(operandos[0].replace(" ", ""));
-        String operando2 = getOperando(operandos[1].replace(" ", ""));;
+        String operando2 = getOperando(operandos[1].replace(" ", ""));
+        String add = "addi";
+        if(operando2.contains("$t")){
+            add = "add";
+        }
         int temporalInt = getTemporalInt(id);
-        text += "add $t"+ temporalInt +"," + operando1 + "," + operando2 + "\n"; 
+        text += add + " $t"+ temporalInt +"," + operando1 + "," + operando2 + "\n"; 
     }
     
     private void restaCase(String id,String p2){
         String[] operandos = p2.split("\\-");
         String operando1 = getOperando(operandos[0].replace(" ", ""));
-        String operando2 = getOperando(operandos[1].replace(" ", ""));;
+        String operando2 = getOperando(operandos[1].replace(" ", ""));
+        String sub = "subi";
+        if(operando2.contains("$t")){
+            sub = "sub";
+        }
         int temporalInt = getTemporalInt(id);
-        text += "sub $t"+ temporalInt +"," + operando1 + "," + operando2 + "\n"; 
+        text += "subi $t"+ temporalInt +"," + operando1 + "," + operando2 + "\n"; 
     }
     
     private void returnCase(String linea){
@@ -163,9 +249,17 @@ public class Generador {
     private void writeCase(String linea){
         if(linea.contains("String_")){ //caso de imprimir un string
             String id = linea.replace("write", "").replace("(", "").replace(")", "")
-                    .replace("String_", "");
+                    .replace("String_", ""); 
             text += "li $v0, 4\n"
                     + "la $a0, " + id + 
+                    "\nsyscall\n";
+        } else if(linea.contains("Int_")) {
+            String id = linea.replace("write", "").replace("(", "").replace(")", "")
+                    .replace("Int_", "");
+            int tempPos = getPosTempOrPila(id);
+            String idMips = "$" + tempPos;
+            text += "li $v0, 1\n"
+                    + "move $a0, " + idMips + 
                     "\nsyscall\n";
         }
     }
@@ -218,8 +312,18 @@ public class Generador {
     }
     
     private int getPosTempInt(String id){
+        System.out.println("getPosTempInt");
+        System.out.println(id);
+        System.out.println(tempToString());
+        id = id.replaceAll(" ", "");
         for (int i = 0; i < temporalesInt.length; i++){
-            if(temporalesInt[i].compareTo(id) == 0){
+            System.out.println(id);
+            System.out.println(temporalesInt[i]);
+            System.out.println("("+id+")("+temporalesInt[i]+")");
+            System.out.println(temporalesInt[i].equals(id));
+            if(temporalesInt[i].equals(id)){
+                System.out.println("return");
+                System.out.println(i);
                 return i;
             }
         }
